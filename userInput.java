9ca0b5ab10c3ca4e5175cf72137527abdf720dba
fullstack251022 <116830126+fullstack251022@users.noreply.github.com>
2022-11-04 12:33:55 +0200

    Create userInput.java

---
+++ userInput.java
@@ -0,0 +1,51 @@
+
+//import
+
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        //        Kumkum kumkum = new Kumkum('Red kumkum, Tefal');
+        //        kumkum = new Kumkum('blue kumkum , LG');
+
+        Scanner s = new Scanner(System.in);
+        System.out.println("Please enter your name");
+        String userInput = s.nextLine();  // "20" + 20
+        System.out.println("Your name is "+userInput);
+
+
+//        Scanner s = new Scanner(System.in);
+//        System.out.println("Please enter number");
+//        int number = s.nextInt();
+//        System.out.println("Your number is: "+number);
+//        number *=2;
+//        System.out.println("The new number is: "+number);
+
+
+//        Scanner s = new Scanner(System.in);
+//        System.out.println("Please enter first number");
+//        int firstNumber = s.nextInt();
+//        System.out.println("Please enter second number");
+//        int secondNumber = s.nextInt();
+//        int sum=firstNumber+secondNumber;
+//        System.out.println("The sum of two numbers is "+sum);
+
+//        Scanner s = new Scanner(System.in);
+//        System.out.println("Please enter your age ");
+//        int age = s.nextInt();
+//        if(age>=18){
+//            System.out.println("You are very big boy/girl");
+//        }else {
+//            System.out.println("You are very small to watch the content");
+//        }
+
+
+
+
+
+
+    }
+}
