039a826b2b809ab570c7f7225d6c93d58f599bc0
fullstack251022 <116830126+fullstack251022@users.noreply.github.com>
2022-11-04 10:56:41 +0200

    Create primitivesAndOperators.java

---
+++ primitivesAndOperators.java
@@ -0,0 +1,63 @@
+public class Main {
+    public static void main(String[] args) {
+
+//        int x = 5;
+//        int y = 10;
+//        byte myByte = 100;
+//        short myShort = 30000;
+//        int myInt = 9223372;
+//        long myLong = 12312312313213123L;
+//        float myFloat = 5.5f;
+//        double myDouble = 123123312.243234234;
+//        char myChar = 'a';
+//        String myString = "This is my text";
+//        boolean isBigger = x > y; //this is Comparison operator
+//        System.out.println(isBigger);
+
+
+//        int x = 5 + 6;
+//        float myFloat = 10f / 4f;
+//        System.out.println(myFloat);
+
+//        int myInt = 11 / 4;
+//          int myInt = 11 % 4;  // 11 / 4 = 2,   2*4 = 8 ,  11 - 8 = 3;
+
+//         String stringOne = "Hello ";
+//         String stringTwo = "World";
+//         String fullString = stringOne + stringTwo;
+//         System.out.println(fullString);
+
+//          int x = 11;
+//          int remainings = x % 2; // Sheerit
+//          boolean isEqual = remainings == 0;
+//          String messageToPrint = "The number is equal? " + isEqual;
+//          System.out.println(messageToPrint);
+
+//        System.out.println(myInt);
+
+
+//        int x = 4;
+//        x++; //x = x + 1; same
+//        x--; //x = x - 1;
+
+//        int x = 5;
+//        x += 10; // x = x + 10;
+//        System.out.println(x);
+
+
+//          x++;
+//          ++x;
+//         int x = 5;
+//         int y = 10 + ++x;
+//
+//        System.out.println(y);
+//        System.out.println(x);
+
+
+//        boolean
+        
+
+    }
+}
+
+
